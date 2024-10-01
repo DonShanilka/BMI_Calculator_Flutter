cada0ffb8c87d4a21af09e283e58170dfaa45140
Shanilka800@# <shanilka799@gmail.com>
2024-10-01 10:12:35 +0530

    add output style

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,4 +2,7 @@ import 'package:flutter/material.dart';
 
 // constenst dhanawanam eka aniwarenma 'k' Akuren thama patran ganna one
 const kTextColor = Color.fromARGB(255, 255, 0, 0);
+
+const kOutPutColor = TextStyle(color: Color.fromARGB(255, 0, 255, 21),fontSize: 60,fontWeight:FontWeight.bold);
+
 const kLableStyle = TextStyle(color: kTextColor, fontSize: 50, fontWeight: FontWeight.bold);
--- lib/main_page.dart
+++ lib/main_page.dart
@@ -13,109 +13,114 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Container(
-          color: const Color.fromARGB(255, 229, 242, 255),
-          child: const Column(
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        Icon(
-                          Icons.male,
-                          size: 150,
-                        ),
-                        Text("Male")
-                      ],
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+            color: const Color.fromARGB(255, 255, 255, 255),
+            child: const Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.male,
+                            size: 150,
+                          ),
+                          Text("Male")
+                        ],
+                      ),
                     ),
-                  ),
-                  Spacer(), // meken wenne me dheka maddata spacer ekak dhanawa
-                  Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        Icon(
-                          Icons.female,
-                          size: 150,
-                        ),
-                        Text("FeMil")
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        Text("Height"),
-                        Text(
-                          "176",
-                          style: kLableStyle,
-                        ),
-                        Row(
-                          children: [
-                            FloatingActionButton(
-                              onPressed: null,
-                              child: Icon(
-                                Icons.add,
-                                size: 40,
+                    Spacer(), // meken wenne me dheka maddata spacer ekak dhanawa
+                    Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.female,
+                            size: 150,
+                          ),
+                          Text("FeMil")
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 50,),
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          Text("Height"),
+                          Text(
+                            "176",
+                            style: kLableStyle,
+                          ),
+                          Row(
+                            children: [
+                              FloatingActionButton(
+                                onPressed: null,
+                                child: Icon(
+                                  Icons.add,
+                                  size: 40,
+                                ),
                               ),
-                            ),
-                            SizedBox(
-                              width: 25,
-                            ),
-                            FloatingActionButton(
-                              onPressed: null,
-                              child: Icon(Icons.remove),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                  Spacer(), // meken wenne me dheka maddata spacer ekak dhanawa
-                  Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        Text("Weight"),
-                        Text(
-                          "76",
-                          style: kLableStyle,
-                        ),
-                        Row(
-                          children: [
-                            FloatingActionButton(
-                              onPressed: null,
-                              child: Icon(
-                                Icons.add,
-                                size: 40,
+                              SizedBox(
+                                width: 25,
                               ),
-                            ),
-                            SizedBox(
-                              width: 25,
-                            ),
-                            FloatingActionButton(
-                              onPressed: null,
-                              child: Icon(Icons.remove),
-                            )
-                          ],
-                        )
-                      ],
+                              FloatingActionButton(
+                                onPressed: null,
+                                child: Icon(Icons.remove),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Column(
-                children: [Text("BMI"), Text("22.00")],
-              )
-            ],
-          )),
+                    Spacer(), // meken wenne me dheka maddata spacer ekak dhanawa
+                    Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          Text("Weight"),
+                          Text(
+                            "76",
+                            style: kLableStyle,
+                          ),
+                          Row(
+                            children: [
+                              FloatingActionButton(
+                                onPressed: null,
+                                child: Icon(
+                                  Icons.add,
+                                  size: 40,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 25,
+                              ),
+                              FloatingActionButton(
+                                onPressed: null,
+                                child: Icon(Icons.remove),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                 SizedBox(height: 50,),
+                 Column(
+                   children: [Text("BMI"), Text("22.00",style: kOutPutColor)],
+                )
+              ],
+            )),
+      ),
     ));
   }
 }
