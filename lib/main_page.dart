6e7e6797a0abcdaaa40f3a2df38874961b322cf0
Shanilka800@# <shanilka799@gmail.com>
2024-10-01 11:19:49 +0530

    update bmi calculate duntion

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -166,6 +166,6 @@ class _MainPageState extends State<MainPage> {
   }
 
   double calculateBMI({required height, required int weight}) {
-    return weight / (height * height);
+    return (weight / (height * height)) * 10000;
   }
 }
